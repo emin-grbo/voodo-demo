d0af0fdd612741b3130ea4f03db5a971561e65d7
emin <emin@grbo.space>
2025-01-13 11:16:54 +0100

    Initial Commit

---
+++ tise-case/tise_caseApp.swift
@@ -0,0 +1,17 @@
+//
+//  tise_caseApp.swift
+//  tise-case
+//
+//  Created by emin on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct tise_caseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
