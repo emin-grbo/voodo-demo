4f3a7a739aca1de2dc2ab701df25edb171450e12
emin <emin@grbo.space>
2025-01-13 13:04:48 +0100

    Add Constants struct

---
+++ tise-case/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Constants {
+  // number of columns on the listing page
+  static let columnCount: Int = 2
+  // spacing between items on the listing page
+  static let columnSpacing: CGFloat = 16
+}
