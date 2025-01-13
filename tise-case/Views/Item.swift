a97860eb65ca4bfe1b4890cf8ce580cf359f083b
emin <emin@grbo.space>
2025-01-13 13:06:03 +0100

    Add Item and Item Detail View

---
+++ tise-case/Views/Item.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct Item: View {
+  
+  let listing: Listing
+  let baseSize: CGFloat
+
+    var body: some View {
+      VStack(alignment: .leading) {
+          AsyncImage(url: URL(string: listing.secondaryImage ?? "")) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: baseSize,
+                           height: baseSize * 1.2)
+            } placeholder: {
+              ZStack {
+                Color.tiseAccent
+                Image(systemName: "photo.badge.exclamationmark")
+                  .foregroundStyle(.white)
+              }
+            }
+            .clipShape(
+              RoundedRectangle(cornerRadius: 12, style: .continuous)
+            )
+            .clipped()
+
+            Text(listing.title)
+                .font(.headline)
+                .lineLimit(1)
+
+        HStack {
+          Text(listing.size)
+            .foregroundColor(.gray)
+          Text("•")
+          Text("\(listing.askingPrice) \(listing.currency)")
+            .font(.subheadline)
+            .bold()
+            .foregroundStyle(Color.tiseAccent)
+        }
+      }
+    }
+}
